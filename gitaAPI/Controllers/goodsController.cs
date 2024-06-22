2b89b498a4d9d1123fbfadff8b6280fd53548ddc
joberhau <janos.oberhauser@gmail.com>
2024-06-22 17:10:38 +0200

    “változások”

--- gitaAPI/Controllers/goodsController.cs
+++ gitaAPI/Controllers/goodsController.cs
@@ -153,6 +153,68 @@ namespace gitaAPI.Controllers
         return new JsonResult(json);
     }
 
+
+    [Authorize(Roles = UserRoles.User)]
+    [HttpPost("getUnits")]
+    [Produces("application/json")]
+    public async Task<IActionResult> getunits()
+    {
+        //String EntityToJson = JsonConvert.SerializeObject(goodsSetGoods);
+        string connMSSQL = _configuration.GetValue<string>("ConnectionStrings:connMSSQL");
+        var accessToken = Request.Headers[HeaderNames.Authorization];
+        var retJson = "";
+        var errorMessage = "";
+        try
+        {
+
+            using (SqlConnection con = new SqlConnection(connMSSQL))
+            {
+                using (SqlCommand cmd = new SqlCommand("getUnits", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@retJSON", SqlDbType.NVarChar, -1).Direction = ParameterDirection.Output;
+                    cmd.Parameters.AddWithValue("@token", accessToken.ToString());
+                 //   cmd.Parameters.AddWithValue("@jsonData", EntityToJson);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+
+                    retJson = (string)cmd.Parameters["@retJSON"].Value;
+                }
+            }
+
+        }
+        catch (Exception e)
+        {
+
+            errorMessage = e.Message;
+
+
+            var errorHandling = new ErrorHandling
+            {
+                error = "error",
+                errormessage = errorMessage
+            };
+
+
+            string jsonString = JsonConvert.SerializeObject(errorHandling);
+
+            retJson = jsonString;
+
+
+        }
+
+        JObject json = JObject.Parse(retJson);
+
+        int errorCode = (int)json["errorCode"];
+
+        if (errorCode == 401)
+        {
+            return StatusCode(StatusCodes.Status401Unauthorized, json);
+        }
+
+        return new JsonResult(json);
+    }
+
 }
 
 }
